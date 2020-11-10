05125207e370962c7bcfaf448654da2d68b879bd
trinhtien2212 <trinhtien6236@gmail.com>
2020-11-10 18:51:45 +0700

    Modified sale.html

--- js/main.js
+++ js/main.js
@@ -22,6 +22,7 @@
             Gallery filter
         --------------------*/
         $('.featured__controls li').on('click', function () {
+            console.log("Co vo");
             $('.featured__controls li').removeClass('active');
             $(this).addClass('active');
         });
@@ -29,8 +30,25 @@
             var containerEl = document.querySelector('.featured__filter');
             var mixer = mixitup(containerEl);
         }
+
+        $('.code_sale_controls li').on('click', function () {
+            console.log('Co vo');
+            $('.code_sale_controls li').removeClass('active');
+            $(this).addClass('active');
+        });
+        if ($('.code_sale_filter').length > 0) {
+            var containerEl1 = document.querySelector('.code_sale_filter');
+            var mixer = mixitup(containerEl1);
+        }
     });
-   
+    // Viet them
+    $('#show_immediately').click(function (e) { 
+        console.log("co vo0-------------");
+        $('#addShow_imme').addClass('mixitup-control-active');
+        
+    });
+    // end viet them
+
     /*------------------
         Background Set
     --------------------*/
@@ -219,6 +237,67 @@
             }
         }
         $button.parent().find('input').val(newVal);
+    });
+
+    // Header sale
+    var ww = $(window).width();
+    $(window).on("resize",function(){
+        ww = $(window).width();
+        if(ww < 980) {
+            $("#sys_mod_filter").removeClass("fix-top");
+            $("#sys_tmp_height_filter").remove();
+        }
+    });
+    var sys_mod_filter = $("#sys_mod_filter");
+    if(sys_mod_filter.length>0) {
+        var filterOffTop = $("#sys_mod_filter").offset().top;
+        $(window).on("scroll",function(){
+            if (ww > 980) {
+                var getOffTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+                if(getOffTop>filterOffTop) {
+                    sys_mod_filter.addClass("fix-top");
+                    if($("#sys_tmp_height_filter").length<=0) {
+                        $("<div id='sys_tmp_height_filter'></div>").height($("#sys_mod_filter").outerHeight(true)).insertAfter("#sys_mod_filter");
+                    }
+                }else{
+                    sys_mod_filter.removeClass("fix-top");
+                    $("#sys_tmp_height_filter").remove();
+                }
+            }
+        });
+    }
+    $("#sys_apply_filter").on("click",function(){
+        var paramAjax = {
+            keyword: $("#sys_txt_search").val(),
+            cateId: $("#sys_selected_val").children("span").attr("data-cate-id"),
+            minDayLeft: $("#sys_filter_days_left").val()[0],
+            maxDayLeft: $("#sys_filter_days_left").val()[1]
+        };
+        console.log(paramAjax);
+    });
+    $("#sys_selected_val").on("click",function(e){
+        $("#sys_list_dd_cate").fadeToggle(300);
+        $("body").on("click.hideDropCate",function(){
+            $("#sys_list_dd_cate").fadeOut(300);
+            $("body").off("click.hideDropCate");
+        });
+        e.stopPropagation();
+    });
+    $("#sys_list_dd_cate").on("click","a",function(e) {
+        $("#sys_selected_val").children("span").html($(this).html()).attr("data-cate-id", $(this).attr("data-cate-id"));
+        $("#sys_list_dd_cate").fadeOut(300);
+        $("body").off("click.hideDropCate");
+        e.stopPropagation();
+        return false;
+    });
+    $("#sys_apply_filter").on("click",function(){
+        var paramAjax = {
+            keyword: $("#sys_txt_search").val(),
+            cateId: $("#sys_selected_val").children("span").attr("data-cate-id"),
+            minDayLeft: $("#sys_filter_days_left").val()[0],
+            maxDayLeft: $("#sys_filter_days_left").val()[1]
+        };
+        console.log(paramAjax);
     });
 
 })(jQuery);
