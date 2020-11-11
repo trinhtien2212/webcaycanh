6bee9e778c07c2b013151b348a9458fed7bd1338
Trần Nhật Thy <73516408+nhatthy24@users.noreply.github.com>
2020-11-11 18:49:42 +0700

    Đang Sửa Blog

--- js/main.js
+++ js/main.js
@@ -185,18 +185,19 @@
         maxamount = $("#maxamount"),
         minPrice = rangeSlider.data('min'),
         maxPrice = rangeSlider.data('max');
+        
     rangeSlider.slider({
         range: true,
         min: minPrice,
         max: maxPrice,
         values: [minPrice, maxPrice],
         slide: function (event, ui) {
-            minamount.val('$' + ui.values[0]);
-            maxamount.val('$' + ui.values[1]);
+            minamount.val(ui.values[0]+'.000 VNĐ');
+            maxamount.val(ui.values[1]+'.000 VNĐ');
         }
     });
-    minamount.val('$' + rangeSlider.slider("values", 0));
-    maxamount.val('$' + rangeSlider.slider("values", 1));
+    minamount.val(rangeSlider.slider("values", 0)+".000 VNĐ");
+    maxamount.val(rangeSlider.slider("values", 1)+".000 VNĐ");
 
     /*--------------------------
         Select
